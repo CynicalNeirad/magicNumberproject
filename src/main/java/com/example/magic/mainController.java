5f3b3a01b76d023e50312f5ef5fab13f6a9fa81c
Richard Spencer <rlspenc15@earlham.edu>
2018-01-26 10:31:38 -0500

    Initial commit

---
+++ src/main/java/com/example/magic/mainController.java
@@ -0,0 +1,29 @@
+package com.example.magic;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import java.util.Scanner;
+@RestController
+public class mainController {
+
+    @RequestMapping("/magicNumber")
+    public String magicNumber() {
+        String storeNumbers = " ";
+        Integer startNumber = 0;
+        Scanner numberInput = new Scanner(System.in);
+        System.out.println("Please Input your number.");
+        startNumber = numberInput.nextInt();
+        int magicNumber = startNumber;
+        do {
+            if (magicNumber % 2 == 0) {
+                magicNumber = (magicNumber/2);
+            }else{
+                magicNumber = (3*magicNumber)+1;
+            }
+            storeNumbers = storeNumbers + magicNumber;
+        }while (magicNumber != 1);
+        System.out.println(storeNumbers);
+    return null;
+
+    }
+}
