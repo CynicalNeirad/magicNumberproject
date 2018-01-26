952cbc75627765334d3c362394c824406a0bb604
Richard Spencer <rlspenc15@earlham.edu>
2018-01-26 13:57:06 -0500

    Added comments to code

--- src/main/java/com/example/magic/mainController.java
+++ src/main/java/com/example/magic/mainController.java
@@ -11,16 +11,18 @@ public class mainController {
     @RequestMapping(value = "/magicNumber" , method = RequestMethod.GET)
     public String magicNumber(
         @RequestParam("startNumber") int startNumber) {
-        String storeNumbers = " ";
-        int magicNumber = startNumber;
+        String storeNumbers = " ";                                               /*Creates a string to add numbers to (gets published at end)*/
+        int magicNumber = startNumber;                                           /*Stores the starting value for final output*/
         do {
-            storeNumbers = storeNumbers + " " + magicNumber + " ";
-            if (magicNumber % 2 == 0) {
+            storeNumbers = storeNumbers + " " + magicNumber + " ";               /*Adds the current number to the string along with proper formatting*/
+            if (magicNumber % 2 == 0) {                                          /*Checks if current number is even and divides that number by two if it is*/
                 magicNumber = (magicNumber / 2);
-            } else {
+            } else {                                                             /*if the number isn't even, then it must be odd and this is multiplied by 3 and then has one added to it.*/
                 magicNumber = (3 * magicNumber) + 1;
             }
-        } while (magicNumber != 1);
-        return ("Your starting number: " + startNumber + "<br/>" + storeNumbers + " 1");
+        } while (magicNumber != 1);                                              /*Closes out the loop when the current number is equal to 1*/
+        return ("Your starting number: " + startNumber + "<br/>" + storeNumbers + " 1");  /*returns the string of numbers with correct formating, and adds one to the end of it as one ends the loop and is thus not added to the string*/
     }
+
+
 }
