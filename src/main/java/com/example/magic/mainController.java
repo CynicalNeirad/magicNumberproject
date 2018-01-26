8ec92d431a0770b11c68abe9d7c9f28ba719d9aa
Richard Spencer <rlspenc15@earlham.edu>
2018-01-26 11:28:01 -0500

    Got webinput working correctly, but due to testing, previous code for console inputs still here.

--- src/main/java/com/example/magic/mainController.java
+++ src/main/java/com/example/magic/mainController.java
@@ -1,29 +1,31 @@
 package com.example.magic;
 
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import java.util.Scanner;
 @RestController
 public class mainController {
 
-    @RequestMapping("/magicNumber")
-    public String magicNumber() {
+    @RequestMapping(value = "/magicNumber" , method = RequestMethod.GET)
+    public String magicNumber(
+        @RequestParam("startNumber") int startNumber) {
         String storeNumbers = " ";
-        Integer startNumber = 0;
-        Scanner numberInput = new Scanner(System.in);
+        //Scanner numberInput = new Scanner(System.in);
         System.out.println("Please Input your number.");
-        startNumber = numberInput.nextInt();
+      //  int magicNumber = numberInput.nextInt();
         int magicNumber = startNumber;
         do {
             storeNumbers = storeNumbers + " " + magicNumber + " ";
             if (magicNumber % 2 == 0) {
-                magicNumber = (magicNumber/2);
-            }else{
-                magicNumber = (3*magicNumber)+1;
+                magicNumber = (magicNumber / 2);
+            } else {
+                magicNumber = (3 * magicNumber) + 1;
             }
-        }while (magicNumber != 1);
-        System.out.println(storeNumbers + " 1");
-    return null;
-
+        } while (magicNumber != 1);
+        //System.out.println(storeNumbers + " 1");
+        return (startNumber + "<br/>" + storeNumbers + " 1");
+    //return null;
     }
 }
